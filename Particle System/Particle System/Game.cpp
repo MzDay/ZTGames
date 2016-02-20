046ebc988d58259e6175348f2424f19b56ab4c3e
Guy Weinberg <weinguy@gmail.com>
2016-02-20 15:26:59 +0200

    - Did a jewish thing

--- Particle System/Particle System/Game.cpp
+++ Particle System/Particle System/Game.cpp
@@ -3,19 +3,25 @@
 void Game::startup() {
 
 	program.createProgram();
-	
+
 	Shader vertex{ GL_VERTEX_SHADER, "vertex.glsl" };
 	Shader fragment{ GL_FRAGMENT_SHADER, "fragment.glsl" };
 
 	program.addShader({ vertex, fragment });
 
-	static const GLfloat positions[] = { -0.5f, -0.5f, 0.0f,
-										  0.5f, -0.5f, 0.0f,
-										  0.0f,  0.5f, 0.0f };
+	static const GLfloat positions[] =
+	{
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f,
+		-0.5f, 0.25f, 0.0f,
+		0.5f, 0.25f, 0.0f,
+		0.0f,  -0.75f, 0.0f,
+	};
 
 	glGenBuffers(1, &buf);
 	glGenVertexArrays(1, &vao);
-	
+
 	glBindBuffer(GL_ARRAY_BUFFER, buf);
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
@@ -36,16 +42,16 @@ void Game::update(double time) {
 }
 
 void Game::render(double time) {
-	
+
 	float r = static_cast<float>(std::sin(time));
 	float g = static_cast<float>(std::cos(time));
 	float b = static_cast<float>(std::sin(time));
 
 	GLfloat black[] = { r, g, b, 0.0f };
 	glClearBufferfv(GL_COLOR, 0, black);
-	
+
 	glBindVertexArray(vao);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
 	glBindVertexArray(0);
 }
 
