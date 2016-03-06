dc5b306cc229f9e0c4ae1a53b27df1347a68db8a
Guy Weinberg <weinguy@gmail.com>
2016-03-07 00:05:51 +0200

    - Added reset button *R* - Changed squares arangment

--- Particle System/Particle System/Game.cpp
+++ Particle System/Particle System/Game.cpp
@@ -104,17 +104,21 @@ void Game::render(double currentTime)
 	for (int i = 0; i < 24; ++i) {
 		float time = (float)(i)+(float)(currentTime)* 0.3f;
 
-		glm::mat4 location_matrix = glm::translate(glm::vec3(0.0f, 0.0f, -10.0f));
+		int isNegativeOffset = (i % 2 == 0) ? 1 : -1;
+		float xOffset = (i % 6) * 3;
+		float yOffset = (i / 6) * 3;
+
+		glm::mat4 location_matrix = glm::translate(glm::vec3(0.0f + xOffset, 0.0f + yOffset, -5.0f * (i % 2)));
 		glm::mat4 move_animaiton_matrix = glm::translate(glm::vec3(
-			sinf(2.1f * time) * i,
-			cosf(2.7f * time) * i,
-			sinf(1.3f * time) * cosf(1.5f * time * i)));
+			sinf(2.1f * time),
+			cosf(2.7f * time),
+			sinf(1.3f * time) * cosf(1.5f * time)));
 
 		glm::mat4 rotate_x_matrix = glm::rotate(glm::radians((float)currentTime * 45.0f + i), glm::vec3(1.0f, 0.0f, 0.0f));
 		glm::mat4 rotate_y_matrix = glm::rotate(glm::radians((float)currentTime * 45.0f + i), glm::vec3(0.0f, 1.0f, 0.0f));
 		glm::mat4 rotate_z_matrix = glm::rotate(glm::radians((float)currentTime * 45.0f + i), glm::vec3(0.0f, 0.0f, 1.0f));
 
-		glm::mat4 mv_matrix = camera_matrix * move_animaiton_matrix * location_matrix;
+		glm::mat4 mv_matrix = camera_matrix * location_matrix * move_animaiton_matrix * rotate_x_matrix * rotate_y_matrix * rotate_z_matrix;
 
 		glUniformMatrix4fv(mv_location, 1, GL_FALSE, glm::value_ptr(mv_matrix));
 
@@ -177,6 +181,10 @@ void Game::userKeyCallback(int key, int action) {
 	}
 	if (key == GLFW_KEY_RIGHT) {
 		camera_matrix *= glm::rotate(float(glm::radians(1.0f)), glm::vec3(0.0f, 1.0f, 0.0f));
+	}
+
+	if (key == GLFW_KEY_R) {
+		camera_matrix = glm::translate(glm::vec3(0.0f, 0.0f, 0.0f));
 	}
 }
 
